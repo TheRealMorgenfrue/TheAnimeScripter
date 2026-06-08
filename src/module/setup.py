8d1aa453c5ac9be6760833b26a0ecad4f6dd4594
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-06-08 02:59:23 +0200

    Add pipeline execution entry point

---
+++ src/module/setup.py
@@ -0,0 +1,26 @@
+import traceback
+from time import time
+
+from applib import LoggingManager
+
+from src.module.utils.inputOutputHandler import IOHandler
+from src.module.video_processor import VideoProcessor
+
+
+def process_arguments():
+    """Execute processing pipeline based on config values."""
+    start_time = time()
+    logger = LoggingManager()
+    input_files = IOHandler().get_input_files()
+    file_count = len(input_files)
+
+    for i, path_config in enumerate(input_files, 1):
+        logger.info(f"({i}/{file_count}) Processing video '{path_config}'")
+        try:
+            VideoProcessor(path_config)
+        except Exception:
+            logger.error(
+                f"Failed to process video '{path_config}'\n{traceback.format_exc()}"
+            )
+
+    logger.info(f"Total execution time: {time() - start_time}")
