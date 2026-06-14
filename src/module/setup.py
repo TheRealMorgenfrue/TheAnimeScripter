c733c835c3504802417b847ee5fd9cabd2d50d94
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-06-14 02:01:28 +0200

    Minor fixes

--- src/module/setup.py
+++ src/module/setup.py
@@ -3,7 +3,7 @@ from time import time
 
 from applib import LoggingManager
 
-from src.module.utils.inputOutputHandler import IOHandler
+from src.module.utils.io_handler import IOHandler
 from src.module.video_processor import VideoProcessor
 
 
@@ -15,12 +15,12 @@ def process_arguments():
     file_count = len(input_files)
 
     for i, path_config in enumerate(input_files, 1):
-        logger.info(f"({i}/{file_count}) Processing video '{path_config}'")
+        logger.info(f"({i}/{file_count}) Processing video '{path_config.input_path}'")
         try:
             VideoProcessor(path_config)
         except Exception:
             logger.error(
-                f"Failed to process video '{path_config}'\n{traceback.format_exc()}"
+                f"Failed to process video '{path_config.input_path}'\n{traceback.format_exc()}"
             )
 
-    logger.info(f"Total execution time: {time() - start_time}")
+    logger.info(f"Total execution time: {time() - start_time:.2f} s")
