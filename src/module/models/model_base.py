de6cf22b7941d1c66077fdd397abe6cc91f6fae7
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-07-20 00:59:37 +0200

    Update model base

--- src/module/models/model_base.py
+++ src/module/models/model_base.py
@@ -5,6 +5,7 @@ from typing import Any
 
 import olive
 import onnxruntime
+import torch
 from applib import LoggingManager
 from torch import Tensor
 
@@ -16,11 +17,60 @@ from src.module.config.tas_config import TASConfig
 class ModelBase:
     """Base class for all models"""
 
-    def __init__(self) -> None:
+    def __init__(
+        self, model_path: Path, inputs: dict[str, Tensor], outputs: dict[str, Tensor]
+    ) -> None:
+        """Initialize a model for inference.
+
+        Parameters
+        ----------
+        model_path : Path
+            The location of the model to initialize.
+        input_names : list[str]
+            The names of the arguments in the model's forward pass.
+
+            For instance, consider the following model's forward pass:
+            ```
+            def forward(self, img0, img1, timestep, f0): ...
+            ```
+            Here, the `input_names` is: `["img0", "img1", "timestep", "f0"]`.
+
+        output_names : list[str]
+            The names of the returned values in the model's forward pass.
+
+            For instance, consider the following return values:
+            ```
+            return (warped_img0 * mask + warped_img1 * (1 - mask))[
+                :, :, : self.height, : self.width
+            ], f1
+            ```
+            Here, the `output_names` would be: `["output", "f1"]`.
+
+        Raises
+        ------
+        RuntimeError
+            If model optimization failed.
+        ValueError
+            If some model data provided by Olive is None.
+        TypeError
+            If input is of incorrect type.
+        """
         self.logger = LoggingManager()
         self.tas_config = TASConfig()
 
-    def auto_tune_parameters(self, model_path: Path) -> olive.ModelOutput | None:
+        if self.tas_config["auto_tune"]:
+            input_model = self._auto_tune_parameters(model_path)  # RuntimeError
+        else:
+            input_model = model_path  # TODO: Convert to ONNX if not one already
+
+        # The session controls inference and is main entrypoint for inference.
+        self.session = self._create_inference_session(input_model)
+        # The IO binding controls input/output tensors for inference and defines dataflow.
+        self.io_binding = self._apply_io_bindings(
+            self.session.io_binding(), *self._declare_io_bindings(input_model)
+        )
+
+    def _auto_tune_parameters(self, model_path: Path) -> olive.ModelOutput:
         """Automatically optimizes the model for inference on the user's hardware.
 
         The model is converted to ONNX format.
@@ -33,8 +83,13 @@ class ModelBase:
 
         Returns
         -------
-        ModelOutput | None
-            The the optimized ONNX model if succesful, `None` otherwise.
+        ModelOutput
+            The the optimized ONNX model.
+
+        Raises
+        ------
+        RuntimeError
+            If model optimization failed.
         """
         olive_config = OliveConfig()
         workflow_output = olive.run(olive_config.get_workflow_config())  # type: ignore # They have outdated type hints (https://microsoft.github.io/Olive/0.12.1/reference/python_api.html)
@@ -65,11 +120,11 @@ class ModelBase:
                 )
             )
         else:
-            self.logger.error(f"Failed to optimize model '{model_path}'")
+            raise RuntimeError(f"Failed to optimize model '{model_path}'")
 
         return best_model
 
-    def create_inference_session(
+    def _create_inference_session(
         self, input_model: Path | olive.ModelOutput
     ) -> onnxruntime.InferenceSession:
         """Creates a ONNX Runtime inference session for the input model.
@@ -134,11 +189,63 @@ class ModelBase:
         session.enable_fallback()
         return session
 
-    @abstractmethod
-    def declare_io_bindings(
+    def _apply_io_bindings(
         self,
+        io_binding: onnxruntime.IOBinding,
+        input_bindings: list[IOBindingConfig],
+        output_bindings: list[IOBindingConfig],
+    ) -> onnxruntime.IOBinding:
+        """Apply the IO binding configurations to the ONNX Runtime IOBinding.
+
+        Parameters
+        ----------
+        io_binding : onnxruntime.IOBinding
+            The ONNX Runtime IOBinding.
+        input_bindings : list[IOBindingConfig]
+            A list of input binding configurations.
+        output_bindings : list[IOBindingConfig]
+            A list of output binding configurations.
+
+        Returns
+        -------
+        onnxruntime.IOBinding
+            The ONNX Runtime IOBinding mapped to the IO configurations.
+        """
+        for ib in input_bindings:
+            io_binding.bind_input(**ib.get_raw())
+        for ob in output_bindings:
+            io_binding.bind_output(**ob.get_raw())
+        return io_binding
+
+    def _declare_io_bindings(
+        self,
+        # inputs: dict[str, Tensor],
+        # outputs: dict[str, Tensor],
+        test: olive.ModelOutput,
     ) -> tuple[list[IOBindingConfig], list[IOBindingConfig]]:
         """Define the input and output tensors used by the model.
+
+        Parameters
+        ----------
+        input_names : list[str]
+            The names of the arguments in the model's forward pass.
+
+            For instance, consider the following model's forward pass:
+            ```
+            def forward(self, img0, img1, timestep, f0): ...
+            ```
+            Here, the `input_names` is: `["img0", "img1", "timestep", "f0"]`.
+
+        output_names : list[str]
+            The names of the returned values in the model's forward pass.
+
+            For instance, consider the following return values:
+            ```
+            return (warped_img0 * mask + warped_img1 * (1 - mask))[
+                :, :, : self.height, : self.width
+            ], f1
+            ```
+            Here, the `output_names` would be: `["output", "f1"]`.
 
         Returns
         -------
@@ -147,7 +254,33 @@ class ModelBase:
             - `tuple[0]` is the list of input tensor bindings.
             - `tuple[1]` is the list of output tensor bindings.
         """
-        ...
+        is_output = False
+        input_configs = []
+        output_configs = []
+        # for d in [inputs, None, outputs]:
+        #     if d is None:
+        #         is_output = True
+        #         continue
+
+        #     for name, tensor in d.items():
+        #         data = {
+        #             "name": name,
+        #             "device_type": "cuda",  # TODO: Make a setting
+        #             "device_id": 0,  # TODO: Auto-select based on device_type setting
+        #             "element_type": self.tas_config["precision"],
+        #             "shape": tensor.shape,
+        #             "buffer_ptr": tensor.data_ptr(),
+        #         }
+
+        #         if is_output:
+        #             output_configs.append(IOBindingConfig(f"{name}_outbinding", data))
+        #         else:
+        #             input_configs.append(IOBindingConfig(f"{name}_inbinding", data))
+
+        conf = test.get_inference_config()
+        print(conf)
+
+        return (input_configs, output_configs)
 
     @abstractmethod
     def __call__(self, frame: Tensor, **kwargs) -> None:
