775b1ef8251076cf88396fb6931566cbec7cc676
TheRealMorgenfrue <33068980+TheRealMorgenfrue@users.noreply.github.com>
2026-07-26 01:03:53 +0200

    Add classes to handle model tensors

---
+++ src/module/models/tensors_base.py
@@ -0,0 +1,116 @@
+from abc import abstractmethod
+
+import torch
+from torch import Tensor
+from torch.types import Device
+
+
+class ModelTensorsBase:
+    """Base class for creating and storing model tensors."""
+
+    def create_io_tensors(
+        self,
+        input_names: list[str],
+        input_types: list[torch.dtype] | torch.dtype,
+        input_shapes: list[list[int]],
+        input_devices: list[Device] | Device,
+        output_names: list[str],
+        output_types: list[torch.dtype] | torch.dtype,
+        output_shapes: list[list[int]],
+        output_devices: list[Device] | Device,
+    ) -> tuple[dict[str, Tensor], dict[str, Tensor]]:
+        """Create input/output tensors for a model.
+
+        Parameters
+        ----------
+        input_names : list[str]
+            The names the arguments in the model's forward pass.
+
+            For instance, consider the following model's forward pass:
+            ```
+            def forward(self, img0, img1, timestep, f0): ...
+            ```
+            Here, `input_names` would be: `["img0", "img1", "timestep", "f0"]`.
+        input_types : list[torch.dtype] | torch.dtype
+            The data types of the tensors defined by `input_names`.
+        input_shapes : list[list[int]]
+            The shapes of the tensors defined by `input_names`.
+        input_devices : list[Device] | Device
+            The devices where tensors defined by `input_names` will be placed.
+        output_names : list[str]
+            The names of the returned values in the model's forward pass.
+
+            For instance, consider the following return values:
+            ```
+            return (warped_img0 * mask + warped_img1 * (1 - mask))[
+                :, :, : self.height, : self.width
+            ], f1
+            ```
+            Here, `output_names` would be: `["output", "f1"]`.
+        output_types : list[torch.dtype] | torch.dtype
+            The data types of the tensors defined by `output_names`.
+        output_shapes : list[list[int]]
+            The shapes of the tensors defined by `output_names`.
+        output_devices : list[Device] | Device
+            The devices where tensors defined by `output_names` will be placed.
+
+        Returns
+        -------
+        tuple[dict[str, Tensor], dict[str, Tensor]]
+            Returns a tuple, where:
+                - tuple[0] are the input tensors.
+                - tuple[1] are the output tensors.
+        """
+        if not isinstance(input_types, list):
+            input_types = [input_types for _ in range(len(input_names))]
+        if not isinstance(input_devices, list):
+            input_devices = [input_devices for _ in range(len(input_names))]
+        if not isinstance(output_types, list):
+            output_types = [output_types for _ in range(len(input_names))]
+        if not isinstance(output_devices, list):
+            output_devices = [output_devices for _ in range(len(input_names))]
+
+        input_tensors: dict[str, Tensor] = {}
+        for iname, itype, ishape, idevice in zip(
+            input_names, input_types, input_shapes, input_devices, strict=True
+        ):
+            input_tensors[iname] = torch.zeros(ishape, dtype=itype, device=idevice)
+
+        output_tensors: dict[str, Tensor] = {}
+        for oname, otype, oshape, odevice in zip(
+            output_names, output_types, output_shapes, output_devices, strict=True
+        ):
+            output_tensors[oname] = torch.zeros(oshape, dtype=otype, device=odevice)
+
+        return (input_tensors, output_tensors)
+
+    @abstractmethod
+    def get_dynamic_axes(self) -> dict[str, dict[str, str]]:
+        """Returns the dynamic axes of the tensors.
+
+        A dynamic axis is the part of a tensor which may change at runtime.
+        For instance, the width and height of a video frame changes depending on the video.
+
+        Example
+        -------
+        Here's an example of dynamic axes.
+
+        ```py
+        {
+            "img0": {"2": "height", "3": "width"},
+            "img1": {"2": "height", "3": "width"},
+            "timestep": {"2": "height", "3": "width"},
+            "f0": {"2": "height", "3": "width"},
+            "output": {"1": "height", "2": "width"},
+        }
+        ```
+
+        Returns
+        -------
+        dict[str, dict[str, str]]
+            The key is the name of the input or output and the value is a dictionary
+            that contains the dynamic axes of the input or output.
+            The key of the value dictionary is the index of the dynamic axis and the
+            value is the name of the dynamic axis.
+        """
+        ...
---
+++ src/module/models/vfi/vfi_tensors_base.py
@@ -0,0 +1,108 @@
+import math
+from typing import override
+
+import torch
+from torch import Tensor
+from torch.types import Device
+
+from src.module.models.tensors_base import ModelTensorsBase
+
+
+class VFIModelTensors(ModelTensorsBase):
+    """Base class for creating and storing VFI tensors."""
+
+    def __init__(
+        self,
+        width: int,
+        height: int,
+        multiplier: int = 32,
+        channels: int = 8,
+    ) -> None:
+        super().__init__()
+        self.width = width
+        self.height = height
+        self.multiplier = multiplier
+        self.channels = channels
+        self.padded_width = self.compute_resolution_padding(width, multiplier)
+        self.padded_height = self.compute_resolution_padding(height, multiplier)
+        self.padding = (0, self.padded_width - width, 0, self.padded_height - height)
+
+        self.I0_IN: Tensor
+        self.I1_IN: Tensor
+        self.TIMESTEP_IN: Tensor
+        self.F0_IN: Tensor
+        self.OUTPUT_OUT: Tensor
+        self.F1_OUT: Tensor
+
+        frame_shape = [1, 3, self.padded_height, self.padded_width]
+        timestep_shape = [1, 1, self.padded_height, self.padded_width]
+        encoded_frame_shape = [1, self.channels, self.padded_height, self.padded_width]
+
+        self.input_names = ["img0", "img1", "timestep", "f0"]
+        self.input_shapes = [
+            frame_shape,  # I0
+            frame_shape,  # I1
+            timestep_shape,  # Timestep
+            encoded_frame_shape,  # F0
+        ]
+        self.output_names = ["output", "f1"]
+        self.output_shapes = [
+            frame_shape,  # Output
+            encoded_frame_shape,  # F1
+        ]
+
+    @override
+    def create_io_tensors(
+        self,
+        input_types: list[torch.dtype] | torch.dtype,
+        input_devices: list[Device] | Device,
+        output_types: list[torch.dtype] | torch.dtype,
+        output_devices: list[Device] | Device,
+    ) -> tuple[dict[str, Tensor], dict[str, Tensor]]:
+        input_tensors, output_tensors = super().create_io_tensors(
+            input_names=self.input_names,
+            input_types=input_types,
+            input_shapes=self.input_shapes,
+            input_devices=input_devices,
+            output_names=self.output_names,
+            output_types=output_types,
+            output_shapes=self.output_shapes,
+            output_devices=output_devices,
+        )
+
+        self.I0_IN = input_tensors[self.input_names[0]]
+        self.I1_IN = input_tensors[self.input_names[1]]
+        self.TIMESTEP_IN = input_tensors[self.input_names[2]]
+        self.F0_IN = input_tensors[self.input_names[3]]
+        self.OUTPUT_OUT = output_tensors[self.output_names[0]]
+        self.F1_OUT = output_tensors[self.output_names[1]]
+
+        return (input_tensors, output_tensors)
+
+    def get_dynamic_axes(
+        self,
+    ) -> dict[str, dict[str, str]]:
+        return {
+            self.input_names[0]: {"2": "height", "3": "width"},
+            self.input_names[1]: {"2": "height", "3": "width"},
+            self.input_names[2]: {"2": "height", "3": "width"},
+            self.input_names[3]: {"2": "height", "3": "width"},
+            self.output_names[0]: {"1": "height", "2": "width"},
+        }
+
+    def compute_resolution_padding(self, frame_axis: int, multiplier: int) -> int:
+        """Returns the frame size, e.g., height, with padding.
+
+        Parameters
+        ----------
+        frame_size : int
+            The size of one axis of the frame in pixels, e.g. 720.
+        multiplier : int
+            The model multiplier, e.g. 64.
+
+        Returns
+        -------
+        int
+            The padded frame size, e.g. 768.
+        """
+        return math.ceil(frame_axis / multiplier) * multiplier
